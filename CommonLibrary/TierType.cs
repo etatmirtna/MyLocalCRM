7c2c49a26d17d6f58827262b1230997995bec17d
Tate Antrim <30962032+etatmirtna@users.noreply.github.com>
2026-04-09 22:08:27 -0400

    Create TierType.cs

---
+++ CommonLibrary/TierType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommonLibrary
+{
+    public enum TierType
+    {
+        [Display(Name = "Platinum Tier")]
+        Platinum,
+        [Display(Name = "Gold Tier")]
+        Gold,
+        [Display(Name = "Silver Tier")]
+        Silver,
+        [Display(Name = "Bronze Tier")]
+        Bronze,
+        [Display(Name = "Unknown")]
+        Unknown
+    }
+}
